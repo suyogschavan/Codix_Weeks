9e677af2e3b23ba45621c68b898837927e02f84d
Suyog Chavan <suyogschavan03@gmail.com>
2021-07-06 11:29:52 +0530

    JWT simple node.js api

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express()
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: "hey there welcome to this api service"
+    });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403); //Forbidden
+        } else {
+            res.json({
+                message: 'post created ...',
+                authData,
+            });
+        }
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'suyog',
+        email: "suyogschavan03@gmail.com",
+    };
+
+    jwt.sign({ user: user }, 'secretkey', (err, token) => {
+        res.json({
+            token,
+        });
+    });
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(' ')[1]
+        req.token = bearerToken
+        next()
+
+    } else {
+        res.sendStatus(403); //Forbidden
+    }
+}
+
+app.listen(3000, (req, res) => {
+    console.log("server started on server 3000");
+
+})
